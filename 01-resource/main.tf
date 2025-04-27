519617f0294e78c4eb8530f7f32cb37aae9f9c64
Gaurav <jadhavgaurav413512@gmail.com>
2025-04-27 15:16:00 +0530

    commit files

---
+++ 01-resource/main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "test-vm"
+  location              = "UK West"
+  resource_group_name   = "project-setup-1"
+  network_interface_ids = ["/subscriptions/344a5bc8-8e2d-4945-810b-881e275ec270/resourceGroups/project-setup-1/providers/Microsoft.Network/networkInterfaces/terraform-testing"]
+  vm_size               = "Standard_B2ls_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+
+  storage_image_reference {
+    id = "/subscriptions/344a5bc8-8e2d-4945-810b-881e275ec270/resourceGroups/project-setup-1/providers/Microsoft.Compute/images/local-devops-pratice"
+  }
+  storage_os_disk {
+    name              = "test-vm-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "test-vm"
+    admin_username = "azuser"
+    admin_password = "devops@12345"
+  }
+}
