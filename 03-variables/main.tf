51f920d44364bc073b8c2885be3c2d3a2773f2cb
Gaurav <jadhavgaurav413512@gmail.com>
2025-05-02 07:24:01 +0530

    commit code

--- 03-variables/main.tf
+++ 03-variables/main.tf
@@ -5,3 +5,48 @@ variable "x" {
 output "X" {
   value = var.x
 }
+
+variable "str" {
+  default = "Hello"
+}
+
+variable "num" {
+  default = 20
+}
+
+variable "mybool" {
+  default = true
+}
+
+output "str"{
+  value = "$(var.str), steve"
+}
+
+output "a" {
+  default = "this is normal variable"
+}
+
+variable "b" {
+  default = [
+    1,
+    2,
+    "abc",
+    false
+  ]
+}
+
+variable "c" {
+  default = {
+    x = 10
+    y = 20
+    z = "abc"
+  }
+}
+
+output "b1" {
+  value = var.b[0]
+}
+
+output "c1" {
+  value = var.c["x"]
+}
